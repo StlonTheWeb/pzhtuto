6ca9058e7424f888d87fc24d8f2d0b45af085a1a
StlonTheWeb <69550907+StlonTheWeb@users.noreply.github.com>
2020-08-12 02:15:19 +0200

    Create menu.js

---
+++ .gitignore/menu.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login("");
+
+var prefix = ("vb!");
+
+client.on("message", (message) => {
+
+    if(message.content === "bonjour") {
+        message.channel.send("Salutations!")
+    }
+
+    if (message.content === prefix + "help"){
+        message.channel.send("Veux-tu de l'aide ?")
+    }
+
+});
